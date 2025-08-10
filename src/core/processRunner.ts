5a5a4ddca97e813ad5e62d42eef3c1c0d61260f5
Kamil Patecki <paatorek@gmail.com>
2025-08-10 04:26:27 +0200

    ENOBUFS fix

--- src/core/processRunner.ts
+++ src/core/processRunner.ts
@@ -1,5 +1,5 @@
 import { execSync } from "child_process";
 
 export function runCommandSync(command: string): void {
-  execSync(command);
+  execSync(command, { stdio: "ignore", maxBuffer: 128 * 1024 * 1024 });
 }
